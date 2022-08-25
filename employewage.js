be66252ad623b319c26dd670270af516f36aff5a
unknown <suntanabhagyashree@gmail.com>
2022-08-25 11:46:33 +0530

    Welcome to Employee Wage program

---
+++ employewage.js
@@ -0,0 +1,9 @@
+console.log("Welcome to employee wage program")
+const IS_PRESENT=1;
+let empcheck=Math.floor(Math.random()*2);
+if(empcheck==IS_PRESENT) {
+    console.log("Employee is Present");
+} else {
+    console.log("Employee is Absent");
+}
+
